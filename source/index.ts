3859c4d28f570311789f8cd3f4e7c6a14c7f471c
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-11-21 18:45:13 +0100

    Moves requestAnimationFrame and setTimeout out of loop

--- source/index.ts
+++ source/index.ts
@@ -48,22 +48,25 @@ const TinyFlip: React.FunctionComponent<Props> = props => {
         element.style.transform = `translate(${-xDiff}px, ${-yDiff}px) scale(1)`;
       } else {
         // If the element has no old position it is new since last render
-        element.style.transform = "scale(0.1)";
+        element.style.transform = "translate(0, 0) scale(0.1)";
       }
 
       positions.current[key] = newPos;
+    });
 
+    raf.current = requestAnimationFrame(() => {
       const dur = props.duration || 500;
       const ease = props.easing || "cubic-bezier(0.3,0,0,1)";
-
-      raf.current = requestAnimationFrame(() => {
+      Object.values(elements.current).forEach(element => {
         element.style.transition = `transform ${dur / 1000}s ${ease}`;
         element.style.transform = "translate(0, 0) scale(1)";
-
-        timer.current = setTimeout(() => clearStyles(element), dur);
       });
+
+      timer.current = setTimeout(() => {
+        Object.values(elements.current).forEach(clearStyles);
+      }, dur);
     });
-  }, [props.children]);
+  }, [props.children, props.duration, props.easing]);
 
   React.useEffect(
     () => () => {
